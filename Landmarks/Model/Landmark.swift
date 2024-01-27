734d92a6428bd9b17720833547f01b0fc1e7c3a9
Russell Gordon <rgordon@lcs.on.ca>
2024-01-26 19:18:51 -0500

    Initial project organization.

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,18 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Russell Gordon on 2024-01-26.
+//
+
+import SwiftUI
+
+struct Landmark: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Landmark()
+}
